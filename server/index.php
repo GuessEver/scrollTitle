fe3d1949979171fa4b8060a2f7844e7c9f765b9b
Zhihao Jiang <guessever@gmail.com>
2016-12-12 18:37:28 +0800

    防止刷新重复提交

--- server/index.php
+++ server/index.php
@@ -1,3 +1,16 @@
+<?php
+if(isset($_POST['text']) && $_POST['text'] !== '') {
+	require_once('connection.php');
+	if(strlen($text) > 30) $text = substr($text, 0, 30);
+	$text = urlencode($_POST['text']);
+	$sql = $pdo->prepare('INSERT INTO `title` (`content`, `status`) VALUES (:content, :status);');
+	$sql->bindValue(':content', $text, PDO::PARAM_STR);
+	$sql->bindValue(':status', 0, PDO::PARAM_INT);
+	$sql->execute();
+	header('Location: index.php?result=success');
+}
+?>
+<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
@@ -13,17 +26,10 @@
 			<p><input type="text" name="text" maxlength="30" autofocus></p>
 			<p><input type="submit" value="发射"></p>
 		</form>
+		<?php
+		if(isset($_GET['result']) && $_GET['result'] === 'success') {
+			echo '<p style="color:red;">弹幕发射成功, ' . date('Y-m-d H:i:s', time()) . '</p>';
+		}
+		?>
 	</body>
 </html>
-<?php
-if(isset($_POST['text']) && $_POST['text'] !== '') {
-	require_once('connection.php');
-	if(strlen($text) > 30) $text = substr($text, 0, 30);
-	$text = urlencode($_POST['text']);
-	$sql = $pdo->prepare('INSERT INTO `title` (`content`, `status`) VALUES (:content, :status);');
-	$sql->bindValue(':content', $text, PDO::PARAM_STR);
-	$sql->bindValue(':status', 0, PDO::PARAM_INT);
-	$sql->execute();
-	echo '<p style="color:red;">弹幕发射成功, ' . date('Y-m-d H:i:s', time()) . '</p>';
-}
-$_POST['text'] = '';
